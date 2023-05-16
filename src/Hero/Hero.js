be4f088caa2537c56a33bf03448602914d0ce21c
Carmen Lopez <carmenbaez2000@gmail.com>
2023-05-16 23:22:16 +0200

    Changes in Hero

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./Hero.css";
 
 const Hero = () => {
-  return <pre>Hero</pre>;
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">At NOUI, we care about safe hygiene</h1>
+        <p className="hero-subtitle">
+          Together with you we build a before urination incontinence care, safe,
+          dry – no urinary infection care for an active daily life for everyone.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
